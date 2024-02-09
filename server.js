4ef4f9864f621f694080f16d74afb5a8e7b74187
Sakina Juzer <sakinajuz136@gmail.com>
2024-02-09 11:26:19 +0400

    ..

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.json());
 app.set('port', 3000);
-app.use((req,res,next) => {
+app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
@@ -17,7 +17,7 @@ let db;
 
 MongoClient.connect('mongodb+srv://sakina:Feb232004@cluster0.fvp5jbf.mongodb.net/', (err, client) => {
     db = client.db('MobileApp');
-    });
+});
 
 app.get('/', (req, res, next) => {
     res.send('Select a collection /collection/messages');
@@ -31,7 +31,7 @@ app.param('collectionName', (req, res, next, collectionName) => {
 
 app.get('/collection/:collectionName', (req, res, next) => {
     req.collection.find({}).toArray((e, results) => {
-        if (e) return next (e);
+        if (e) return next(e);
         res.send(results);
     })
 });
@@ -48,39 +48,34 @@ app.post('/:collectionName', (req, res, next) => {
 const ObjectID = require("mongodb").ObjectID;
 
 app.get('/collection/:collectionName/:id', (req, res, next) => {
-    req.collection.findOne({_id: new ObjectID(req.params.id)},
-    (e, result) => {
-        if (e) next(e)
-        res.send(result)
-    })
+    req.collection.findOne({ _id: new ObjectID(req.params.id) },
+        (e, result) => {
+            if (e) next(e)
+            res.send(result)
+        })
 });
 
 app.put('/collection/:collectionName/:id', (req, res, next) => {
     req.collection.update(
-        {_id: new ObjectID(req.params.id)},
-        {$set: req.body},
-        {safe: true, multi: false},
+        { _id: new ObjectID(req.params.id) },
+        { $set: req.body },
+        { safe: true, multi: false },
         (e, result) => {
             if (e) return next(e)
-            res.send((result.result.n === 1) ? {msg : "success"} : {msg : "error"})
+            res.send((result.result.n === 1) ? { msg: "success" } : { msg: "error" })
         }
     );
 });
 
 app.delete('/collection/:collectionName/:id', (req, res, next) => {
     req.collection.deleteOne(
-        {_id: new ObjectID(req.params.id)}, (e, result) => {
+        { _id: new ObjectID(req.params.id) }, (e, result) => {
             if (e) return next(e)
-            res.send((result.result.n === 1) ? {msg : "success"} : {msg : "error"});
+            res.send((result.result.n === 1) ? { msg: "success" } : { msg: "error" });
         }
     );
 });
-
-// http.createServer((req, res) => {
-//     res.setHeader("Access-Control-Allow-Origin", "*");
-//     res.writeHead(200, {"Content-Type" : "text/plain"});
-//     res.end(JSON.stringify(products));
-// });
+ 
 
 const port = process.env.PORT || 3000
 app.listen(port)
